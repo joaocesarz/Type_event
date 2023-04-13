d0c20a9ee8a1c172bafe8a47ad8031aa6350f556
joaocesarz <mercadinhodoleo95@gmail.com>
2023-04-13 19:59:57 -0300

    Type_event

--- type_event/settings.py
+++ type_event/settings.py
@@ -1,7 +1,6 @@
-
-
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'eventos',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +118,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Messages
+
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info ',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django .http import HttpResponse
-
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.urls import reverse
+from django.contrib import auth
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senhas não coincidem.')
+            return redirect(reverse('cadastro')) 
+        
+        # TODO: validar força da senha
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+            return redirect(reverse('cadastro'))
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
+        
+        return redirect(reverse('login'))
+    
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos.')
+            return redirect(reverse('login'))
+        
+        auth.login(request, user)
+        return redirect('/eventos/novo_evento/')
