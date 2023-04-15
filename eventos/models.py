b09128752714cdff18be300628b729dc40a2bc7c
joaocesarz <mercadinhodoleo95@gmail.com>
2023-04-14 23:15:32 -0300

    Adicionando ManyToManyField, muitos p/ muitos para evento_participante

--- eventos/models.py
+++ eventos/models.py
@@ -9,6 +9,8 @@ class Evento(models.Model):
     data_termino = models.DateField()
     carga_horaria = models.IntegerField()
     logo = models.ImageField(upload_to="logos")
+    participantes = models.ManyToManyField(User, related_name="evento_participante", null=True, blank=True)
+
 
     #Paleta de cores
     cor_principal = models.CharField(max_length=7)
