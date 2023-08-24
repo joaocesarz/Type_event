a721801517311986b844f1894dce032a05f26584
joaocesarz <mercadinhodoleo95@gmail.com>
2023-08-24 10:55:21 -0300

    Implementando match

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,44 +7,50 @@ from django.urls import reverse
 from django.contrib import auth
 
 def cadastro(request):
-    if request.method == "GET":
-        return render(request, 'cadastro.html')
-    elif request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-        confirmar_senha = request.POST.get('confirmar_senha')
+    match request.method:
 
-        if not senha == confirmar_senha:
+        case "GET":
+            return render(request, 'cadastro.html')
+        
+        case "POST":
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            senha = request.POST.get('senha')
+            confirmar_senha = request.POST.get('confirmar_senha')
+               
+            if not senha == confirmar_senha:
             messages.add_message(request, constants.ERROR, 'Senhas não coincidem.')
             return redirect(reverse('cadastro')) 
-        
-        # TODO: validar força da senha
 
-        user = User.objects.filter(username=username)
+            # TODO: validar força da senha
 
-        if user.exists():
-            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
-            return redirect(reverse('cadastro'))
+            user = User.objects.filter(username=username)
+
+            if user.exists():
+                messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+                return redirect(reverse('cadastro'))
+            
+
+            user = User.objects.create_user(username=username, email=email, password=senha)
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
         
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
-        
-        return redirect(reverse('login'))
-    
+            return redirect(reverse('login'))
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username')
-        senha = request.POST.get('senha')
-
-        user = auth.authenticate(username=username, password=senha)
-
-        if not user:
-            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos.')
-            return redirect(reverse('login'))
+    match request.method:
         
-        auth.login(request, user)
-        return redirect('/eventos/novo_evento/')
+        case 'GET':
+            return render(request, 'login.html')
+        
+        case 'POST':
+            username = request.POST.get('username')
+            senha = request.POST.get('senha')
+
+            user = auth.authenticate(username=username, password=senha)
+
+            if not user:
+                messages.add_message(request, constants.ERROR, 'Username ou senha inválidos.')
+                return redirect(reverse('login'))
+        
+            auth.login(request, user)
+            return redirect('/eventos/novo_evento/')
